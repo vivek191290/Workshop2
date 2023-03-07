ff443a71050b3c90ce40771ef6a262264caa0710
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-07 08:55:58 +0530

    Refector: selected Number result is stored in dictionary

--- skipNumberProgram.sh
+++ skipNumberProgram.sh
@@ -1,4 +1,5 @@
 echo "Welcome to skip Number program "
+declare -A dict
 
 for i in {1..10}
 do
@@ -7,6 +8,12 @@ case $i in
     3|5|6|8)
       continue;; 
     *)
-      echo $i;; 
+      echo $i
+;; 
+  esac
+case $i in
+    1|2|4|7|9|10)
+      dict[$i]=$i
+;; 
   esac
 done
